66c08c7488b0f46095f52c2e557c1bf914cd0277
Dalia Osama <dalyaasamh19@gmail.com>
2025-09-24 22:31:50 +0300

    Update 11. Container With Most Water.cpp

--- Solutions/leet code/array/11. Container With Most Water.cpp
+++ Solutions/leet code/array/11. Container With Most Water.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    int maxArea(vector<int>& he) {
+     int n = he.size();
+        int maxi = INT_MIN;
+        int st = 0; 
+        int en = n -1;
 
+        while(st <= en)
+        {
+            int width = en - st;
+            int ht = min(he[st], he[en]);
+            int area = width * ht;
+            maxi = max(maxi, area);
+
+            he[st] > he[en] ? en-- : st++;
+        }
+
+        return maxi;
+        
+    }
+};
